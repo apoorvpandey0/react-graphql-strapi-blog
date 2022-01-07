0dbad3e7d1e1fc26654f04c9b4c49869d8e03c1a
apoorvpandey0 <apoorvpandey0@gmail.com>
2022-01-08 01:00:56 +0530

    images and details working now

--- src/pages/blog-details.js
+++ src/pages/blog-details.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 export default function BlogDetails() {
+    var params = useParams();
+    var { data, error, loading } = useFetch('http://localhost:1337/api/blog-posts/?populate=*' + params.id);
+    
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error...</div>
+    
+    console.log(data);
+    var post = data;
     return (
         <div>
-            Blog Details
+                <div key={post.id} className='blog-card'> 
+                    <h1 className='rating'>{post.id}</h1>
+                    <h1>{post.attributes.title} </h1>
+                    <h6>{post.attributes.createdAt}</h6>
+                    <img width="auto" height="400" src={"http://localhost:1337"+post.attributes.image.data.attributes.url} alt="hi" />
+                    <p>{post.attributes.content} ...</p>
+                </div>
         </div>
     )
 }
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
 
 export default function Homepage() {
-    const { data, error, loading } = useFetch('http://localhost:1337/api/blog-posts')
+    const { data, error, loading } = useFetch('http://localhost:1337/api/blog-posts/?populate=*')
 
     if (loading) return <div>Loading...</div>
     if (error) return <div>Error...</div>
@@ -11,12 +11,12 @@ export default function Homepage() {
         <div>
             {data.map(post=>(
                 <div key={post.id} className='blog-card'> 
+                    <h1 className='rating'>{post.id}</h1>
                     <h1>{post.attributes.title} </h1>
                     <h6>{post.attributes.createdAt}</h6>
-                    <img src={post.attributes.image} alt="" srcset="" />
-                    <p>{post.attributes.content}</p>
+                    <img width="auto" height="400" src={"http://localhost:1337"+post.attributes.image.data.attributes.url} alt="hi" />
+                    <p>{post.attributes.content.substring(0,200)} ...</p>
                     <Link to={`/details/${post.id}`}>Read more</Link>
-
                 </div>
             ))}
         </div>
