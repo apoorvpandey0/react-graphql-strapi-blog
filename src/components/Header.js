0a1f9b8bd0d49e4ae7bf35fd1a6caf7483315952
apoorvpandey0 <apoorvpandey0@gmail.com>
2022-01-08 15:05:52 +0530

    integrated graphql with categories

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
+import {useQuery,gql} from '@apollo/client'
+import { Link } from 'react-router-dom'
+
+const GET_CATEGORIES = gql`
+query GetCategories{
+    categories{
+      data{
+        id,
+        attributes{
+          title
+        }
+    }
+}
+}`;
+
 export default function Header() {
+    const {loading,error,data} = useQuery(GET_CATEGORIES);
+    console.log(data);
+
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error...</div>
     return (
-        <div>
-            Site Header
+        <div className='site-header'>
+            <Link to="/"><h1>Strapi React GraphQL Blogs</h1></Link>
+            <nav className='categories'>
+                <span>Filter reviews by category: </span>
+                {data.categories.data.map(category =>(
+                <Link key={category.id} to={`/category/${category.id}`}>{category.attributes.title}</Link>
+            ))}
+            </nav>            
         </div>
     )
 }
--- src/pages/categorys.js
+++ src/pages/categorys.js
@@ -1,9 +1,72 @@
 import React from 'react'
 
+import { Link, useParams } from 'react-router-dom';
+import { useQuery,gql } from '@apollo/client';
+
+const GETCATEGORY_POSTS = gql`
+query GetCategory($id:ID!){
+    category(id:$id){
+      data{
+        id
+        attributes{
+          title
+          blogs{
+            data{
+              id
+              attributes{
+                title
+                excerpt
+                content
+                createdAt
+                categories{
+                  data{
+                    id
+                    attributes{
+                      title
+                    }
+                  }
+                }
+                image{
+                  data{
+                    attributes{
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+
 export default function Categorys() {
+    var params = useParams();
+    const {loading,error,data} = useQuery(GETCATEGORY_POSTS,{variables:{id:params.id}});
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>Error...</div>
+    console.log(data.category.data.attributes);
+
     return (
         <div>
-            Category
+            <h2>{data.category.data.attributes.title}</h2>
+            {data.category.data.attributes.blogs.data.map(post=>(
+                <div key={post.id} className='blog-card'> 
+                    <h1 className='rating'>{post.id}</h1>
+                    <h1>{post.attributes.title} </h1>
+                    <h6>{post.attributes.createdAt}</h6>
+                    <img  src={"http://localhost:1337"+post.attributes.image.data.attributes.url} alt="hi" />
+                    <p className = "categories">Categories: {post.attributes.categories.data.map(c=>(
+                        <span>
+                            <Link to={`/category/${c.id}`}>{c.attributes.title}</Link>
+                        </span>
+                    ))}</p> 
+                    <p>{post.attributes.content.substring(0,200)} ...</p>
+                    <Link to={`/details/${post.id}`}>Read more</Link>
+                </div>
+            ))}
         </div>
     )
 }
